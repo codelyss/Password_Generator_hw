14849ab854c512a2fad488f2a42ba3e976d827ff
codelyss <atcodeveloper@gmail.com>
2019-12-09 21:15:53 -0500

    Define characters and copy button. Made copy button disabled by default

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var copyBtn = document.querySelector("#copy");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+     var password = generatePassword();
+     var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+     passwordText.value = password;
 
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
+     copyBtn.removeAttribute("disabled");
+     copyBtn.focus();
 }
 
+// Length (Must be between 8 - 128 characters) 
+
+// Special characters 
+var specialChars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+// Numeric characters 
+var numChars = "0123456789";
+
+// Lowercase characters 
+var lowChars = "abcdefghijklmnopqrstuvwxyz";
+
+// Uppercase characters 
+var uppChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
 function copyToClipboard() {
-  // BONUS 
+    // BONUS 
 }
 
 // Add event listener to generate button
